f4190d125c22cb235398e047016ab1d57f6552e7
gandori <marcel.stiebing@web.de>
2023-06-10 02:49:26 +0200

    add app.js && app.sass

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './app.sass';
+
+const App = () => {
+  const theme = 'black'
+
+  return (
+    <div id='app' className={theme}>
+      <h1>React Template</h1>
+    </div>
+  );
+};
+
+
+export default App
