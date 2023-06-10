ea07b9776f088d69f342b6ee1c18e275396a070d
gandori <marcel.stiebing@web.de>
2023-06-10 02:51:16 +0200

    update index.js && add index.sass && themes.sass

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './app';
+import './index.sass';
+import './themes.sass';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>ReactTemplate</h1>);
+root.render(<App></App>);
