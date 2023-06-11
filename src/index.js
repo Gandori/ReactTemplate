4655625fbf01acbc6580408d187415d22fc8d453
gandori <marcel.stiebing@web.de>
2023-06-11 06:02:42 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app';
 import './index.sass';
 import './themes.sass';
 
+const App = lazy(() =>
+    import(/* webpackChunkName: "app" */ './app')
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App></App>);
